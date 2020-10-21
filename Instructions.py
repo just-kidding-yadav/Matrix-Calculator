a3d0c715cb486bea894db3c73923b3e2d0aca5f6
Jitender <jkyadav1006@gmail.com>
2020-10-21 10:43:12 +0530

    First Commit

---
+++ Instructions.py
@@ -0,0 +1,12 @@
+def inst():
+    print("Please Note the Following:")
+    print("Type I for instructions.")
+    print("1. Type N to create a new matrix.")
+    print("2. Type A to add two matrices as a new matrix.")
+    print("3. Type M to multiply two matrices as a new matrix")
+    print("4. Type T to store the transpose of a matrix.")
+    print("5. Type D to find the determinant of a matrix.")
+    print("6. Type O to get the order of the matrix.")
+    print("Type P to print a matrix")
+    print("Type Q to quit")
+    
---
+++ Matrix.py
@@ -0,0 +1,103 @@
+import copy
+
+def mat_def():
+    name = str(input("Name of the matrix: "))
+    m1 = int(input("Enter total number of rows in the matrix: "))
+    n1 = int(input("Enter total number of columns in the matrix: "))
+
+    New = [[0 for col in range(n1)]for row in range(m1)]
+
+    print("Enter all elements one after other.")
+    for i in range(m1):
+        for j in range(n1):
+            New[i][j] = int(input(name + str(i+1) + str(j+1)))
+    return (name, New)
+
+def mat_print(Mat, p):
+    m1 = len(A)
+    n1 = len(A[1])
+    m2 = len(B)
+    n2 = len(B[1])
+    
+    print("\n" + "Matrix" + str(p) + "is")
+    for i in range(m1):
+        for j in range(n1):
+            print(Mat[p][i][j], end = " ")
+        print()
+    print()
+
+def add(A, B):
+    name = str(input("Name of the new matrix is: "))
+    m1 = len(A)
+    n1 = len(A[1])
+    m2 = len(B)
+    n2 = len(B[1])
+
+    if(m1 == m2 and n1 == n2):
+        Out = [[0 for col in range(n1)]for row in range(m1)]
+        for i in range(m2):
+            for j in range(n2):
+                Out[i][j] = A[i][j] + B[i][j]
+        return (name, Out)
+            
+    else:
+        print("Addition is not Defined")
+        return (None, None)
+
+def mul(A, B):
+    name = str(input("Name of the new matrix is: "))
+    m1 = len(A)
+    n1 = len(A[1])
+    m2 = len(B)
+    n2 = len(B[1])
+
+    if(m2 == n1):
+        product = [[0 for row in range(n2)]for col in range(m1)]
+        for i in range(m1):
+            for j in range(n2):
+                prod[i][j]=0
+                for k in range(m2):
+                    prod[i][j]+=(A[i][k])*(B[k][j])
+        return (name, prod)
+            
+    else:
+        print("Multiplication is not Defined.")
+        return (None, None)
+
+def tnp(A):
+    name = str(input("Name of the new matrix is: "))
+    m1 = len(A)
+    n1 = len(A[1])
+    
+    comp = [[0 for col in range(m1)]for row in range(n1)]
+    for i in range(n1):
+        for j in range(m1):
+            comp[i][j] = A[j][i]
+    return (name, comp)
+
+def det(A):
+    p = len(A)
+    determinant = 0
+    if p == 1:
+        determinant = int(A[0][0])
+            
+    if p != 1:
+        determinant = 0
+        adj = list()
+
+        # Expanding along Row 1
+        
+        for i in range(p):
+            lst = A
+            B = copy.deepcopy(A)
+            x = B.pop(0)
+            
+            for j in range(p-1):
+                x = B[j].pop(i)
+                            
+            adj.append(det(B))
+                    
+        for i in range(p):
+            determinant += (A[0][i] * adj[i] * ((-1) ** (i)))
+    
+    return determinant
